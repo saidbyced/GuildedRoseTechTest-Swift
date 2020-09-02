18fe48829a2a57cce13b25b703ada9db395b2f62
Chris Eadie <chris.eadie@outlook.com>
2020-09-02 13:55:36 +0100

    Corrected new updateQuality function to remove incorrect logic and removed old function

--- GuildedRoseTechTest-Swift/GuildedRose.swift
+++ GuildedRoseTechTest-Swift/GuildedRose.swift
@@ -15,95 +15,45 @@ public class GildedRose {
     
     public func updateQuality() {
         for item in items {
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
+            if item.name != "Sulfuras, Hand of Ragnaros" {
+                switch item.name {
+                case "Aged Brie":
+                    if item.quality < 50 {
+                        item.quality += 1
                     }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                    
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                        
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality < 50 {
+                        switch item.sellIn {
+                        case 0:
+                            item.quality = 0
+                        case 1...5:
+                            item.quality += 3
+                        case 6...10:
+                            item.quality += 2
+                        default:
+                            item.quality += 1
                         }
                     }
-                }
-            }
-            
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
-            }
-            
-            if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
-                            }
+                default:
+                    if item.quality != 0 {
+                        var adjustment = 1
+
+                        if item.sellIn <= 0 {
+                            adjustment *= 2
                         }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
+
+                        if item.description.lowercased().contains("conjured") {
+                            adjustment *= 2
+                        }
+
+                        if item.quality - adjustment < 0 {
+                            item.quality = 0
+                        } else {
+                            item.quality -= adjustment
+                        }
                     }
                 }
             }
         }
     }
-    
-//    public func updateQuality2() {
-//        for item in items {
-//            if item.quality != 0 && item.name != "Sulfuras, Hand of Ragnaros" {
-//                switch item.name {
-//                case "Aged Brie":
-//                    if item.quality == 50 {
-//                        return
-//                    } else {
-//                        item.quality += 1
-//                    }
-//                case "Backstage passes to a TAFKAL80ETC concert":
-//                    if item.quality == 50 {
-//                        return
-//                    } else {
-//                        switch item.sellIn {
-//                        case 0:
-//                            item.quality = 0
-//                        case 1...5:
-//                            item.quality += 3
-//                        case 6...10:
-//                            item.quality += 2
-//                        default:
-//                            item.quality += 1
-//                        }
-//                    }
-//                default:
-//                    var adjustment = 1
-//
-//                    if item.sellIn <= 0 {
-//                        adjustment *= 2
-//                    }
-//
-//                    if item.description.lowercased().contains("conjured") {
-//                        adjustment *= 2
-//                    }
-//
-//                    item.quality -= adjustment
-//                }
-//            }
-//        }
-//    }
 }
